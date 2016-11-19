b630bd6508f89ef921ee6bd546a3429c711fe9a6
ranggarmaste <ranggarmaste@gmail.com>
2016-11-19 23:49:39 +0700

    Merge

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import weka.core.Attribute;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils;
+
+import java.util.Enumeration;
 
 /**
  * Created by ranggarmaste on 11/16/16.
@@ -13,6 +17,21 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        System.out.println(data);
+        /*** AWAL TEST. ENTAR HAPUS AJA ***/
+        // Ambil instance
+        Enumeration<Instance> enu = data.enumerateInstances();
+        while (enu.hasMoreElements()) {
+            Instance inst = enu.nextElement();
+            for (int i = 0; i < inst.numAttributes(); i++) {
+                if (i != inst.classIndex()) {
+                    double val = inst.value(i); // Dapat nilai atribut ke-i
+                }
+            }
+        }
+        // Mau ambil nama atribut
+        /*** AKHIR TEST ***/
 
         ANN ann = new ANN();
         String[] options = new String[6];
